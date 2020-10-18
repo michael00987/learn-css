735a9ce8b1ca65701b702ce011b665f98a2014b7
michael00987 <hyeongkwangseok@gmail.com>
2020-10-18 19:38:56 +0900

    fix star style

--- creation/03. transform_effects_on_scroll/index.js
+++ creation/03. transform_effects_on_scroll/index.js
@@ -3,5 +3,5 @@ const banner=document.querySelector('.banner')
 window.addEventListener('scroll',(e)=>{
   const value = window.scrollY
   console.log(value)
-  banner.style.clipPath=`polygon(50% ${0-value/4}%, ${82+value/4}% ${100+value/2}%, ${0-value/4}% ${38-value/4}%, ${100+value/4}% ${38-value/4}%, ${18-value/4}% ${100+value/2}%)`
+  banner.style.clipPath=`polygon(50% 0%, ${61+value/12}% ${35-value/12}%, 100% 35%, ${68+value/12}% ${57+value/12}%, 79% 100%, 50% ${70+value/12}%, 21% 100%, ${32-value/12}% ${57+value/12}%, 0% 35%, ${39-value/12}% ${35-value/12}%)`
 })
